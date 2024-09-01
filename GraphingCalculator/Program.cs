5ed73fc54385b122769dd08c5a297bb65a784078
Adam Polner <polnera@rsd407.org>
2024-09-01 16:35:47 -0700

    Add project files.

---
+++ GraphingCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System;
+
+namespace GraphingCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
